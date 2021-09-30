c0ba8606ce2da7cc294eead25c07e3ed712c3a5a
chan0nel <shvakova2004@gmail.com>
2021-09-30 21:54:36 +0300

    ui: add new structure

--- lib/presentation/lecture.dart
+++ lib/presentation/lecture.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_poci_book/presentation/lecture_list.dart';
 import 'package:flutter_poci_book/presentation/home.dart';
 
@@ -12,39 +13,89 @@ class _LectureState extends State<Lecture>{
   @override
     Widget build(BuildContext context) {
       return Scaffold(
-        body: Container(
-          color: Color(0xFF6200EE),
-          padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Color(0xFF6200EE),
-            selectedItemColor: Colors.white,
-            unselectedItemColor: Colors.white.withOpacity(.60),
-            onTap: (value) {
-              if (value == 0);
-              if (value == 1){
-                Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const LectureList()));
-              };
-              if (value == 2);
-            },
-            items:[
-                    BottomNavigationBarItem(
-              title: Text(''),
-              icon: Icon(Icons.arrow_back_ios_rounded),
-            ),
-            BottomNavigationBarItem(
-              title: Text(''),
-              icon: Icon(Icons.menu),
-            ),
-            BottomNavigationBarItem(
-              title: Text(''),
-              icon: Icon(Icons.arrow_forward_ios_rounded),
-            )
-            ]
-          )
-      ));
+        body: Stack(
+          children: [
+            Container(
+              color: Colors.green[300],
+              padding: EdgeInsets.only(top: 30),
+                  child: Stack(
+                    children: [
+                          Row(
+                            children: [IconButton(
+                              onPressed: (){
+                                Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) => const LectureList()));
+                              },
+                              icon: Icon(Icons.menu_sharp),
+                            ),
+                            Text("Теория")]),
+                      
+                          Container(
+                            padding: EdgeInsets.only(top: 25),
+                            child: TextFormField(
+                            initialValue: 'Input text',
+                            decoration: InputDecoration(
+                            prefix: IconButton(
+                              onPressed: (){},
+                              icon: Icon(Icons.search_sharp)
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                      )),
+                  ])),
+                Center(
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    margin: EdgeInsets.only(top: 125),
+                    child: Expanded(
+                      child: PageView(
+                      scrollDirection: Axis.vertical,
+                      children: <Widget>[
+                        LectureCard(),
+                        LectureCard(),
+                        LectureCard()
+                      ],
+                    )
+                  )),
+                  )
+                ]
+                )
+      );
   }
 }
 
+class LectureCard extends StatefulWidget{
+  const LectureCard({Key? key}) : super(key: key);
+  @override
+  _LectureCardState createState() => _LectureCardState();
+}
+
+class _LectureCardState extends State<LectureCard>{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10), 
+      decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              color: Colors.white24,
+              border: Border.all(color: Colors.black)
+
+            ),
+      child: InkWell(
+        splashColor: Colors.black,
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Text("Лекция 1"),
+              Divider(),
+              Text("Многа текста")
+            ],
+          )
+        )
+
+      ));
+  }
+}
