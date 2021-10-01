1fc72eb2b6b3a204f095044b9f8a9265d3b8f4f0
chan0nel <shvakova2004@gmail.com>
2021-10-01 23:16:13 +0300

    redactor: recreate lecture menu

--- lib/presentation/lecture.dart
+++ lib/presentation/lecture.dart
@@ -9,93 +9,55 @@ class Lecture extends StatefulWidget {
   _LectureState createState() => _LectureState();
 }
 
-class _LectureState extends State<Lecture>{
+class _LectureState extends State<Lecture> {
   @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: Stack(
-          children: [
-            Container(
-              color: Colors.green[300],
-              padding: EdgeInsets.only(top: 30),
-                  child: Stack(
-                    children: [
-                          Row(
-                            children: [IconButton(
-                              onPressed: (){
-                                Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) => const LectureList()));
-                              },
-                              icon: Icon(Icons.menu_sharp),
-                            ),
-                            Text("Теория")]),
-                      
-                          Container(
-                            padding: EdgeInsets.only(top: 25),
-                            child: TextFormField(
-                            initialValue: 'Input text',
-                            decoration: InputDecoration(
-                            prefix: IconButton(
-                              onPressed: (){},
-                              icon: Icon(Icons.search_sharp)
-                            ),
-                            enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
-                          ),
-                        ),
-                      )),
-                  ])),
-                Center(
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    margin: EdgeInsets.only(top: 125),
-                    child: Expanded(
-                      child: PageView(
-                      scrollDirection: Axis.vertical,
-                      children: <Widget>[
-                        LectureCard(),
-                        LectureCard(),
-                        LectureCard()
-                      ],
-                    )
-                  )),
-                  )
-                ]
-                )
-      );
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: CustomScrollView(slivers: <Widget>[
+      SliverAppBar(
+          elevation: 0.0,
+          centerTitle: true,
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu_sharp)),
+          title: FlexibleSpaceBar(
+            title: Container(
+              child: Text("Теория", style: TextStyle(color: Colors.black)),
+              margin: EdgeInsets.only(top: 15, left: 16),
+            ),
+          )),
+      SliverList(
+          delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return LectureCard();
+        },
+        childCount: 30,
+      ))
+    ]));
   }
 }
 
-class LectureCard extends StatefulWidget{
+class LectureCard extends StatefulWidget {
   const LectureCard({Key? key}) : super(key: key);
   @override
   _LectureCardState createState() => _LectureCardState();
 }
 
-class _LectureCardState extends State<LectureCard>{
+class _LectureCardState extends State<LectureCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 10), 
-      decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(15.0)),
-              color: Colors.white24,
-              border: Border.all(color: Colors.black)
-
-            ),
-      child: InkWell(
-        splashColor: Colors.black,
-        child: Container(
-          alignment: Alignment.center,
-          child: Column(
-            children: [
-              Text("Лекция 1"),
-              Divider(),
-              Text("Многа текста")
-            ],
-          )
-        )
-
-      ));
+        width: 50,
+        height: 200,
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            border: Border.all(color: Colors.black38)),
+        child: InkWell(
+            child: Container(
+                alignment: Alignment.center,
+                child: Column(
+                  children: [Text("Лекция 1"), Divider(), Text("Многа текста")],
+                ))));
   }
 }
