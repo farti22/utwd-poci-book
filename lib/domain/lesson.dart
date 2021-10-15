787bae21c701c831e7201cc73019ff96e815d132
chan0nel <shvakova2004@gmail.com>
2021-10-15 10:34:59 +0300

    feat: add firebase class

--- lib/domain/lesson.dart
+++ lib/domain/lesson.dart
@@ -1,23 +1,41 @@
-// import 'package:meta/meta.dart';
-
-// abstract class LessonRepository {
-//   Future<Lesson>? getLesson({@required int? number});
-// }
-
-// class Lesson {
-//   final String? title;
-//   final String? description;
-
-//   Lesson({@required this.title, @required this.description});
-// }
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class DBService{
+class Lesson{
 
-  final CollectionReference test = FirebaseFirestore.instance.collection('test');
+  late String id;
+  late String name;
+  //late String description;
+  late String content;
 
-  Stream<QuerySnapshot> get tests{
-    return test.snapshots();
+  Lesson({
+    required this.id,
+    required this.name,
+    //required this.description,
+    required this.content
+  });
+}
+
+class DatabaseService {
+
+  
+  final lectureCollection = FirebaseFirestore.instance.collection("lectures");
+
+  Future<List<Lesson>> getData() async {
+    
+    List<Lesson> listData = [];
+    await lectureCollection.get().then((querySnapshot){
+      for (var doc in querySnapshot.docs) {
+      
+        Lesson lesson = Lesson( // the first issue
+          id: doc.id,
+          name: doc.data()['Name'],
+          //description: doc['Description'],
+          content: doc.data()['Content'],// ?? "Pusto",
+        );
+        listData.add(lesson); // the second issue
+      }
+    });
+    return listData;
   }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_poci_book/application.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_poci_book/domain/lesson.dart';
+
+List<Lesson> listData = [];
+
 
 void main() async {
+  
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
+  await DatabaseService().getData().then((value) => listData = value);
   runApp(const Application());
 }
