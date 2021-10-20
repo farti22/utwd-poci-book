b9cf210aeb3b80e9fa88f03b4e70a76e12be2138
farti22 <artur.maza33@gmail.com>
2021-10-20 12:12:49 +0300

    feat: add constructor fields

--- lib/widgets/utils/outline_textfield.dart
+++ lib/widgets/utils/outline_textfield.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class OutlinedTextField extends StatefulWidget {
   final String labelText;
   final String hintText;
+  final TextEditingController? controller;
+  final IconButton? suffixIcon;
+  final bool readOnly;
+  final List<TextInputFormatter>? textInputFormatter;
   final void Function(String)? onChanged;
 
   const OutlinedTextField({
     Key? key,
     required this.labelText,
     required this.hintText,
-    required this.onChanged,
+    this.readOnly = false,
+    this.onChanged,
+    this.controller,
+    this.suffixIcon,
+    this.textInputFormatter,
   }) : super(key: key);
 
   _OutlinedTextFieldState createState() => _OutlinedTextFieldState();
@@ -21,11 +30,15 @@ class _OutlinedTextFieldState extends State<OutlinedTextField> {
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: widget.controller,
       onChanged: widget.onChanged,
+      readOnly: widget.readOnly,
+      inputFormatters: widget.textInputFormatter,
       decoration: InputDecoration(
         labelText: widget.labelText,
         border: const OutlineInputBorder(),
         hintText: widget.hintText,
+        suffixIcon: widget.suffixIcon,
       ),
     );
   }
