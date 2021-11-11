60d344acee1146e3826a78d245e281eeb95b1c4d
chan0nel <shvakova2004@gmail.com>
2021-11-11 12:07:03 +0300

    ui: change view of cards

--- lib/widgets/lecture/card.dart
+++ lib/widgets/lecture/card.dart
@@ -21,34 +21,55 @@ class _LectureCardState extends State<LectureCard> {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => widget.searchText == "NULL"
-                      ? Lecture(index: widget.index)
-                      : LectureWithSearch(
-                          index: widget.index,
-                          searchText: widget.searchText,
-                          indexList: widget.indexList)));
-        },
-        child: Container(
-            height: 250,
-            margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                border: Border.all(color: Colors.black38)),
-            child: Column(
-              children: [
-                Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 15, 0, 10),
-                    child: Text("Лекция №${listData[widget.index].id}",
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(fontSize: 20))),
-                const Divider(),
-                const Text("Нажми на меня!", style: TextStyle(fontSize: 14))
-              ],
-            )));
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => widget.searchText == "NULL"
+                ? Lecture(index: widget.index)
+                : LectureWithSearch(
+                    index: widget.index,
+                    searchText: widget.searchText,
+                    indexList: widget.indexList),
+          ),
+        );
+      },
+      child: Container(
+        height: 230,
+        alignment: Alignment.bottomCenter,
+        margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+          border: Border.all(color: Colors.blue, width: 2.5),
+        ),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(50, 15, 50, 10),
+              decoration: const BoxDecoration(color: Colors.blue),
+              child: Text(
+                "Лекция №${listData[widget.index].id}",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+              child: Text(
+                listData[widget.index].name,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
